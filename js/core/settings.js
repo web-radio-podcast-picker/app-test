04512edee0070ac0e76fae0c433e85533dc50280
franck gaspoz <franck.gaspoz@gmail.com>
2025-11-15 18:15:08 +0100

    upd

--- js/core/settings.js
+++ js/core/settings.js
@@ -14,9 +14,9 @@ window.settings = {
             verDate: '8/23/2025'
         },
         wrp: {
-            version: '1.3.37',
-            verDate: '11/14/2025',
-            description: 'upgrade footer v2, fix switch fav'
+            version: '1.3.38',
+            verDate: '11/15/2025',
+            description: 'add drag region'
         }
     },
 
